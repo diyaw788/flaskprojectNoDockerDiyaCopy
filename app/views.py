a229a6f26010d18c2d93a1b4dec38d10bc752722
diyaw788 <102425424+diyaw788@users.noreply.github.com>
2024-11-06 22:01:33 -0500

    commit 2

--- app/views.py
+++ app/views.py
@@ -24,6 +24,139 @@ CURRENT_UID = "1234"
 def successPage():
     return render_template('successPage.html')
 
+@app.route('/viewCourseStudents/<int:courseOfferingID>/<string:courseName>')
+def viewCourseStudents(courseOfferingID, courseName):
+    # Use courseID and courseName as needed
+    return render_template('success.html')
+    # return render_template('LUCASPAGE.html', courseOfferingID=courseOfferingID, courseName=courseName)
+
+
+@app.route('/get_courses', methods = ['GET']) 
+def get_courses():
+    try:
+        professorName = request.args.get("professorName")
+        professorID = request.args.get("professorID")
+        print(professorName)
+        print(professorID)
+        sql = '''
+            SELECT [peer-eval-db].dbo.CourseOfferings.COID, Semester, Year, Section, Course_Name FROM [peer-eval-db].dbo.ProfessorCourse 
+            JOIN [peer-eval-db].dbo.CourseOfferings ON [peer-eval-db].dbo.ProfessorCourse.COID=[peer-eval-db].dbo.CourseOfferings.COID
+            JOIN [peer-eval-db].dbo.Courses ON [peer-eval-db].dbo.Courses.Course_ID=[peer-eval-db].dbo.CourseOfferings.Course_ID
+            WHERE Professor_ID = ?;
+        '''
+        cursor.execute(sql, (professorID,))
+
+        courses = cursor.fetchall()
+        print("these are the courses")
+        print(courses)
+        # (1, 'Fall', 2024, 'A', 'Financial Accounting')
+        courses = [
+            {
+                "COID": row[0],
+                "Semester": row[1],
+                "Year": row[2],
+                "Section": row[3],
+                "Course_Name": row[4]
+            }
+            for row in courses
+        ]
+        print("i reach here")
+        return jsonify(courses)
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/sign_in', methods=['GET', 'POST'])
+def sign_in():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        role = request.form.get('role')
+
+        try:
+            if role == 'professor':
+                sql = "SELECT * FROM Professor WHERE Professor_Email = ? AND Password = ?"
+            elif role == 'student':
+                sql = "SELECT * FROM Student WHERE Email = ? AND Password = ?"
+
+            # Query the database for the user with the provided email and password
+            cursor.execute(sql, (email, password))
+            user = cursor.fetchone()
+            print("this is the user:", user)
+            if user:
+                # Convert the Row object to a dictionary
+                user_dict = {
+                    'ID': int(user[0]),           # Ensure ID is an integer
+                    'Name': str(user[1]),         # Ensure Name is a string
+                    'Email': str(user[2]),        # Ensure Email is a string
+                    'Password': str(user[3]),     # Ensure Password is a string
+                    'Role': str(role)             # Ensure Role is a string
+                }
+                print(user_dict)
+                # Store user information in session
+                session['user'] = user_dict
+
+                user_role = session['user'].get('Role')
+                if user_role == 'professor':
+                    return render_template('professor_home.html', user=user_dict)
+                elif user_role == 'student':
+                    return render_template('student_home.html', user=user)
+            else:
+                flash('Incorrect email or password. Please try again.', 'error')
+                return redirect(url_for('sign_in'))
+        except Exception as e:
+            print(f"Error: {e}")
+            flash('An error occurred: {}'.format(str(e)), 'error')
+            return redirect(url_for('sign_in'))
+    else:
+        return render_template('sign_in.html')
+
+@app.route('/professor_home', methods=['GET', 'POST'])
+def professor_home():
+    return render_template('professor_home.html')
+    
+@app.route('/student_home', methods=['GET', 'POST'])
+def student_home():
+    return render_template('student_home.html')
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        role = request.form.get('role')
+        print(name) 
+        # Diya Wadhera
+        print("this is the role:", role)
+        # professor
+
+    
+        
+        if role == 'professor':
+            sql = '''
+                    INSERT INTO Professor (Professor_ID, Professor_Name, Professor_Email, Password)
+                    VALUES (?, ?, ?, ?)
+                '''
+            cursor.execute('SELECT MAX(Professor_ID) FROM Professor')
+        elif role == 'student':
+            sql = '''
+                    INSERT INTO Student (Student_ID, Student_Name, Email, Password)
+                    VALUES (?, ?, ?, ?)
+                '''
+            cursor.execute('SELECT MAX(Student_ID) FROM Student')
+        
+        max_id = cursor.fetchone()[0]
+        if max_id is None:
+            max_id = 0
+        new_id = max_id + 1
+        cursor.execute(sql, (new_id, name, email, password))    
+        conn.commit()
+
+        return render_template('sign_in.html')
+    else:
+        return render_template('sign_up.html')
+
 @app.route('/')
 def indexFunction():
     return render_template('studentLogin.html')
@@ -108,7 +241,7 @@ def index():
 # def test2(beingEval, evaluatorName):
 #     return render_template('test2.html', beingEval=beingEval, evaluatorName=evaluatorName)
  
- 
+
 @app.route('/get_evaluations', methods = ['GET']) 
 def get_evaluations():
     try:
